5954f958660c912f8956ff4ea38fdf972e1e1ac0
Josue Majano <josue_majano@yahoo.com>
2020-12-08 00:31:15 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+
+var cors0ptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(cors0ptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.")
+});
+
+app.get("/", (req, res) => {
+    res.json({message: "Welcome to Josue's application."});
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
